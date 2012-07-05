b38b0fbbcc0c37b109d4deea10ac0af870e5b066
Rob Mooney <mooneyster@gmail.com>
2012-07-05 16:00:38 +0100

    Initial Commit

---
+++ TreeSearchTests/TreeSearchTests.h
@@ -0,0 +1,13 @@
+//
+//  TreeSearchTests.h
+//  TreeSearchTests
+//
+//  Created by Robert Mooney on 05/07/2012.
+//  Copyright (c) 2012 Robert Mooney. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TreeSearchTests : SenTestCase
+
+@end
---
+++ TreeSearchTests/TreeSearchTests.m
@@ -0,0 +1,32 @@
+//
+//  TreeSearchTests.m
+//  TreeSearchTests
+//
+//  Created by Robert Mooney on 05/07/2012.
+//  Copyright (c) 2012 Robert Mooney. All rights reserved.
+//
+
+#import "TreeSearchTests.h"
+
+@implementation TreeSearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TreeSearchTests");
+}
+
+@end
